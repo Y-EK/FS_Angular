b2a1febf3cb50f3c6cc5fb62203a5f4aee984a64
Youness <elkhatabi.youness@gmail.com>
2020-10-11 18:43:07 +0200

    create an example of list of parkings

--- src/app/parkings/parkings.component.ts
+++ src/app/parkings/parkings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ParkingInfo } from '../parkinginfo';
 
 @Component({
   selector: 'app-parkings',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParkingsComponent implements OnInit {
 
+  parkings: ParkingInfo[];
   constructor() { }
 
   ngOnInit(): void {
+    let parking1: ParkingInfo = {
+      id: 1,
+      nom: 'parking 1',
+      nbPlacesDispo: 150,
+      nbPlacesTotal: 200,
+      statut: 'OUVERT',
+      heureMaj: '16h 15'
+    }
+    let parking2: ParkingInfo = {
+      id: 2,
+      nom: 'parking 2',
+      nbPlacesDispo: 300,
+      nbPlacesTotal: 500,
+      statut: 'FERME',
+      heureMaj: '09h 30'
+    } 
+    this.parkings = []; // Initialisation de this.parking !
+    this.parkings.push(parking1);
+    this.parkings.push(parking2);
   }
 
 }
