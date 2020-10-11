c1965d92508da8a68210946d8fb84aabdd37c9c9
Youness <elkhatabi.youness@gmail.com>
2020-10-11 22:05:25 +0200

    create getParkings method

--- src/app/parking.service.ts
+++ src/app/parking.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { ParkingInfo } from './parkinginfo';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,27 @@ import { Injectable } from '@angular/core';
 export class ParkingService {
 
   constructor() { }
+
+  getParkings(): ParkingInfo[]{
+    let parking1: ParkingInfo = {
+      id: 1,
+      nom: 'parking 1',
+      nbPlacesDispo: 150,
+      nbPlacesTotal: 200,
+      statut: 'OUVERT',
+      heureMaj: '16h 15'
+    }
+    let parking2: ParkingInfo = {
+      id: 2,
+      nom: 'parking 2',
+      nbPlacesDispo: 300,
+      nbPlacesTotal: 500,
+      statut: 'FERME',
+      heureMaj: '09h 30'
+    }
+    let parkings = [];
+    parkings.push(parking1);
+    parkings.push(parking2);
+    return parkings; 
+  }
 }
