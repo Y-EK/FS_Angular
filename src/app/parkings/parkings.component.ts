77fceda7f69356d0540e7a92a0f580e8f513b2cf
Youness <elkhatabi.youness@gmail.com>
2020-10-11 22:08:49 +0200

    apply ParkingService s method

--- src/app/parkings/parkings.component.ts
+++ src/app/parkings/parkings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ParkingInfo } from '../parkinginfo';
+import { ParkingService } from '../parking.service';
 
 @Component({
   selector: 'app-parkings',
@@ -9,28 +10,11 @@ import { ParkingInfo } from '../parkinginfo';
 export class ParkingsComponent implements OnInit {
 
   parkings: ParkingInfo[];
-  constructor() { }
+
+  constructor(private parkingService: ParkingService) { }
 
   ngOnInit(): void {
-    let parking1: ParkingInfo = {
-      id: 1,
-      nom: 'parking 1',
-      nbPlacesDispo: 150,
-      nbPlacesTotal: 200,
-      statut: 'OUVERT',
-      heureMaj: '16h 15'
-    }
-    let parking2: ParkingInfo = {
-      id: 2,
-      nom: 'parking 2',
-      nbPlacesDispo: 300,
-      nbPlacesTotal: 500,
-      statut: 'FERME',
-      heureMaj: '09h 30'
-    } 
-    this.parkings = []; // Initialisation de this.parking !
-    this.parkings.push(parking1);
-    this.parkings.push(parking2);
+    this.parkings = this.parkingService.getParkings();
   }
 
 }
