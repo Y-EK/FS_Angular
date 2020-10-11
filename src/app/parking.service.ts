13e4c130a67683e2cae3fa95c961186dde5f6438
Youness <elkhatabi.youness@gmail.com>
2020-10-11 23:20:37 +0200

    update: use get method

--- src/app/parking.service.ts
+++ src/app/parking.service.ts
@@ -1,33 +1,18 @@
 import { Injectable } from '@angular/core';
 import { ParkingInfo } from './parkinginfo';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ParkingService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
-  getParkings(): ParkingInfo[]{
-    let parking1: ParkingInfo = {
-      id: 1,
-      nom: 'parking 1',
-      nbPlacesDispo: 150,
-      nbPlacesTotal: 200,
-      statut: 'OUVERT',
-      heureMaj: '16h 15'
-    }
-    let parking2: ParkingInfo = {
-      id: 2,
-      nom: 'parking 2',
-      nbPlacesDispo: 300,
-      nbPlacesTotal: 500,
-      statut: 'FERME',
-      heureMaj: '09h 30'
-    }
-    let parkings = [];
-    parkings.push(parking1);
-    parkings.push(parking2);
-    return parkings; 
+  getParkings(): Observable<ParkingInfo[]> {
+    
+    return this.httpClient.get<ParkingInfo[]>('http://localhost:8080/api/parkings'); 
+
   }
 }
