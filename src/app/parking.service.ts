d213ee0f6d1fa5b6ad972a4b4438bfb521cb2d97
Youness <elkhatabi.youness@gmail.com>
2020-10-16 21:41:27 +0200

    create method for getting each parking

--- src/app/parking.service.ts
+++ src/app/parking.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ParkingInfo } from './parkinginfo';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -14,6 +15,14 @@ export class ParkingService {
   getParkings(): Observable<ParkingInfo[]> {
     
     return this.httpClient.get<ParkingInfo[]>(environment.apiUrl + '/parkings'); 
+
+  }
+
+  getParking(id: number): Observable<ParkingInfo> {
+    
+    return this.httpClient.get<ParkingInfo[]>(environment.apiUrl + '/parkings').pipe(
+      map(parkings => parkings.find(parking => parking.identifiant === id))
+    ); 
 
   }
 }
