930b1ea31b3d7f2e275134681eaf6b8fa4819cd8
Youness <elkhatabi.youness@gmail.com>
2020-10-16 20:50:10 +0200

    add parking detail route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ParkingDetailComponent } from './parking-detail/parking-detail.component';
 import { ParkingsComponent } from './parkings/parkings.component';
 
 
 const appRoutes: Routes = [
   { path: 'parkings', component: ParkingsComponent },
-  { path: '**', redirectTo: "/parkings"}
+  { path: 'parking/:id', component: ParkingDetailComponent},
+  { path: '**', redirectTo: "/parkings"},
 ];
 
 @NgModule({
