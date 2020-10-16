4d5525566b96be4966d17504536496f1b0066cd6
Youness <elkhatabi.youness@gmail.com>
2020-10-16 21:44:52 +0200

    add getting a parking from the route

--- src/app/parking-detail/parking-detail.component.ts
+++ src/app/parking-detail/parking-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ParkingService } from '../parking.service';
+import { ParkingInfo } from '../parkinginfo';
 
 @Component({
   selector: 'app-parking-detail',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParkingDetailComponent implements OnInit {
 
-  constructor() { }
+  parking: ParkingInfo;
+  constructor(private route: ActivatedRoute, private parkingService: ParkingService) { }
 
   ngOnInit(): void {
+    const parkingId = parseInt(this.route.snapshot.paramMap.get('id'));
+    this.parkingService.getParking(parkingId).subscribe(
+      reponse => {
+        this.parking = reponse;
+      }
+    );
   }
 
 }
